761155979df619f5a6ec52375e54ccd67f0eb9ea
shash-2106 <raoshashwati@gmail.com>
2021-04-08 10:58:19 +0530

    p72

--- screens/WriteStoryScreen.js
+++ screens/WriteStoryScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
+import {StyleSheet, Text, View, TextInput, TouchableOpacity, KeyboardAvoidingView, ToastAndroid, Alert} from 'react-native';
 import db from '../config';
 import firebase from 'firebase';
 
@@ -15,36 +15,38 @@ export default class WriteStoryScreen extends React.Component{
     }
 render(){
     return(
+        <KeyboardAvoidingView behavior="padding" enabled>
         <View>
             
                 <TouchableOpacity  style={styles.box}>
-       <Text style={styles.text}>Title</Text>
-        <TextInput style={styles.inputBox,styles.text}
+       <Text style={styles.text}>Write title below</Text> 
+       </TouchableOpacity>  
+
+        <TextInput style={styles.inputBox,styles.text1}
         value={this.state.title}
         onChangeText={(text)=>(this.setState({
             title:text
         }))}
-        >
-           
-        </TextInput>
-        </TouchableOpacity>
-       
-        
-            <TouchableOpacity style={styles.box}>
-                <Text style={styles.text}> Author</Text>
-            <TextInput style={styles.inputBox,styles.text}
+        >           
+        </TextInput> 
+        <TouchableOpacity style={styles.box}>
+                <Text style={styles.text}> Author - write below</Text>
+                </TouchableOpacity>
+
+            <TextInput style={styles.inputBox,styles.text1}
             value={this.state.author}
             onChangeText={(text)=>{this.setState({
                 author:text
             })}}
-            >
-           
+            >           
             </TextInput>
-            </TouchableOpacity>
+           
        
         <TouchableOpacity style={styles.box}>
-            <Text style={styles.text}>Write story</Text>
-        <TextInput style={styles.inputBox,styles.text} multiline={true}
+            <Text style={styles.text}>Write story below</Text>
+            </TouchableOpacity>
+
+        <TextInput style={styles.inputBox,styles.text1} multiline={true}
         value={this.state.story}
         onChangeText={(text)=>this.setState({
             story:text
@@ -53,64 +55,77 @@ render(){
         >
            
         </TextInput>
-        </TouchableOpacity>
+       
       
       
         <TouchableOpacity style={styles.button} onPress={()=>{this.submitStory()}}>
             <Text style={styles.text}>Submit</Text>
         </TouchableOpacity>
+       
+        
+           
       
         </View>
-
+        </KeyboardAvoidingView>
     )
 }
 submitStory=async()=>{
+    
    db.ref("title").update({"title":this.state.title});
    db.ref("author").update({"author":this.state.author});
    db.ref("story").update({"story":this.state.story});
+   var submitMessage = "Story and details submitted";
+  ToastAndroid.show(submitMessage, ToastAndroid.SHORT);
+        
 }
+
+
 }
+
 const styles = StyleSheet.create({
     box:{
         width:'50%',
     backgroundColor:'black',
-    alignContent:'CENTER',
-    justifyContent:'CENTER',
-    alignSelf:'CENTER',
+    alignContent:'center',
+    justifyContent:'center',
+    alignSelf:'center',
     margin:10,
     borderRadius:5,
     height:50,
-    alignItems:'CENTER'
+    alignItems:'center'
        
     },
     inputBox:{
-        width:'50%',
-    backgroundColor:'black',
-    alignContent:'CENTER',
-    justifyContent:'CENTER',
-    alignSelf:'CENTER',
-    margin:10,
-    borderRadius:5,
-    height:30,
-    alignItems:'CENTER'
+       
+       margin:10,
+    borderWidth:5,
+    height:100,
+    width:300,
+    alignItems:'center',
+    alignSelf:'center',
     },
     text:{
         color:"white",
-        justifyContent:'CENTER',
-       alignSelf:'CENTER',
+        justifyContent:'center',
+       alignSelf:'center',
     
     },
     button:{
         backgroundColor:"blue",
         width:'45%',
         height:20,
-        justifyContent:'CENTER',
-        alignSelf:'CENTER',
-        alignItems:'CENTER',
-        alignContent:'CENTER',
+        justifyContent:'center',
+        alignSelf:'center',
+        alignItems:'center',
+        alignContent:'center',
         borderRadius:5,
         padding:10,
         margin:10,
-        textAlign: 'CENTER'
+        textAlign: 'center'
+    },
+    text1:{
+        color:"black",
+        justifyContent:'center',
+       alignSelf:'center',
     }
 })
